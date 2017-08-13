e7c3480773350db25934d12659ebcdbbab54ce94
Strideradu <dunan00001@hotmail.com>
2017-08-13 18:17:55 -0400

    go back to old optimization script

--- lightgbm_baseline/f1_optimal.py
+++ lightgbm_baseline/f1_optimal.py
@@ -1,110 +1,59 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-from subprocess import check_output
-
-print(check_output(["ls", "../input"]).decode("utf8"))
-
-# Any results you write to the current directory are saved as output.
-
-import itertools
 import pandas as pd
 import numpy as np
+from joblib import Parallel, delayed
+import multiprocessing
 
+from utils import fast_search
+from f1_optimizer import generate_prediction
+#none_product = 50000
 
-class F1Optimizer():
-    def __init__(self):
-        pass
+def applyParallel(dfGrouped, func):
+    retLst = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(func)(group) for name, group in dfGrouped)
+    return pd.concat(retLst)
 
-    @staticmethod
-    def get_expectations(P, pNone=None):
-        expectations = []
-        P = np.sort(P)[::-1]
-
-        n = np.array(P).shape[0]
-        DP_C = np.zeros((n + 2, n + 1))
-        if pNone is None:
-            pNone = (1.0 - P).prod()
-
-        DP_C[0][0] = 1.0
-        for j in range(1, n):
-            DP_C[0][j] = (1.0 - P[j - 1]) * DP_C[0, j - 1]
-
-        for i in range(1, n + 1):
-            DP_C[i, i] = DP_C[i - 1, i - 1] * P[i - 1]
-            for j in range(i + 1, n + 1):
-                DP_C[i, j] = P[j - 1] * DP_C[i - 1, j - 1] + (1.0 - P[j - 1]) * DP_C[i, j - 1]
-
-        DP_S = np.zeros((2 * n + 1,))
-        DP_SNone = np.zeros((2 * n + 1,))
-        for i in range(1, 2 * n + 1):
-            DP_S[i] = 1. / (1. * i)
-            DP_SNone[i] = 1. / (1. * i + 1)
-        for k in range(n + 1)[::-1]:
-            f1 = 0
-            f1None = 0
-            for k1 in range(n + 1):
-                f1 += 2 * k1 * DP_C[k1][k] * DP_S[k + k1]
-                f1None += 2 * k1 * DP_C[k1][k] * DP_SNone[k + k1]
-            for i in range(1, 2 * k - 1):
-                DP_S[i] = (1 - P[k - 1]) * DP_S[i] + P[k - 1] * DP_S[i + 1]
-                DP_SNone[i] = (1 - P[k - 1]) * DP_SNone[i] + P[k - 1] * DP_SNone[i + 1]
-            expectations.append([f1None + 2 * pNone / (2 + k), f1])
-
-        return np.array(expectations[::-1]).T
-
-    @staticmethod
-    def maximize_expectation(P, pNone=None):
-        expectations = F1Optimizer.get_expectations(P, pNone)
-
-        ix_max = np.unravel_index(expectations.argmax(), expectations.shape)
-        max_f1 = expectations[ix_max]
-
-        predNone = True if ix_max[0] == 0 else False
-        best_k = ix_max[1]
-
-        return best_k, predNone, max_f1
-
-    @staticmethod
-    def _F1(tp, fp, fn):
-        return 2 * tp / (2 * tp + fp + fn)
-
-    @staticmethod
-    def _Fbeta(tp, fp, fn, beta=1.0):
-        beta_squared = beta ** 2
-        return (1.0 + beta_squared) * tp / ((1.0 + beta_squared) * tp + fp + beta_squared * fn)
-
-
-from tqdm import tqdm
-
-
-def create_products_faron(df):
+def create_products(df):
+    # print(df.product_id.values.shape)
     products = df.product_id.values
     prob = df.prediction.values
-
+    """
     sort_index = np.argsort(prob)[::-1]
-    prob = prob[sort_index]
-    products = products[sort_index]
 
-    opt = F1Optimizer.maximize_expectation(prob)
 
-    best_prediction = ['None'] if opt[1] else []
-    best_prediction += [str(p) for p in products[:opt[0]]]
-    f1_max = opt[2]
+    values = fast_search(prob[sort_index][0:80], dtype=np.float64)
 
-    best = ' '.join(best_prediction)
-    return (df.iloc[0, 0], best)
+    index = np.argmax(values)
 
+    print('iteration', df.shape[0], 'optimal value', index)
+
+    index, bool_none = generate_prediction(prob)
+
+    if bool_none:
+        best = "None"
+    else:
+        best = ' '.join(map(lambda x: str(x), products[sort_index][0:index]))
+    """
+    best = generate_prediction(prob, products)
+    best = ' '.join(map(lambda x: str(x), best))
+    df = df[0:1]
+    df.loc[:, 'products'] = best
+
+
+    return df
 
 if __name__ == '__main__':
-    data = pd.read_pickle('data/prediction_rnn.pkl')
+    data = pd.read_pickle('/mnt/home/dunan/Learn/Kaggle/instacart/data/prediction_lgbm.pkl')
+    """
+    data['not_a_product'] = 1. - data.prediction
+
+    gp = data.groupby('order_id')['not_a_product'].apply(lambda x: np.multiply.reduce(x.values)).reset_index()
+    gp.rename(columns={'not_a_product': 'prediction'}, inplace=True)
+    gp['product_id'] = none_product
+
+    data = pd.concat([data, gp], axis=0)
+    data.product_id = data.product_id.astype(np.uint32)
 
     data = data.loc[data.prediction > 0.01, ['order_id', 'prediction', 'product_id']]
-    out = [create_products_faron(group) for name, group in tqdm(data.groupby(data.order_id))]
+    """
+    data = applyParallel(data.groupby(data.order_id), create_products).reset_index()
 
-    data = pd.DataFrame(data=out, columns=['order_id', 'products'])
-
-    data.to_csv('data/sub.csv', index=False)
+    data[['order_id', 'products']].to_csv('/mnt/home/dunan/Learn/Kaggle/instacart/data/sub.csv', index=False)
